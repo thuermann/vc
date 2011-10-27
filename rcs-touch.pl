f02bf9528227bd8ef82e6a114ddd46310a527220
Urs Thuermann <urs@isnogud.escape.de>
2011-10-27 15:39:14 +0200

    Find and change $Id$ keywords

--- rcs-touch.pl
+++ rcs-touch.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# $Id: rcs-touch.pl,v 1.2 2011/10/19 21:10:19 urs Exp $
+# $Id: rcs-touch.pl,v 1.3 2011/10/27 13:39:14 urs Exp $
 #
 # Change check-in time of RCS and CVS files.
 
@@ -17,13 +17,20 @@ if (`date +%Y $date_opt` < 2000) {
 } else {
     $date_fmt = "+%Y.%m.%d.%H.%M.%S";
 }
+$id_fmt = "+%Y/%m/%d\\ %T";
 
 $new_date = `date -u $date_fmt $date_opt`;
+$new_id   = `date -u $id_fmt   $date_opt`;
 chop($new_date);
+chop($new_id);
 
-print "$new_date\n";
+print "new date: $new_date\n";
+print "new Id:   $new_id\n";
 
-$re_date = '(\d\d)?\d\d\.\d\d\.\d\d\.\d\d\.\d\d\.\d\d';
+$re_rev  = '\d+\.\d+(\.\d+\.\d+)*';
+$re_date = '(\d\d)?\d\d(\.\d\d){5}';
+$key     = 'Id:';  # Prevent CVS expanding this keyword
+$re_id   = qr/\$$key .* ($re_rev) (\d\d\d\d\/\d\d\/\d\d \d\d:\d\d:\d\d).*\$/;
 
 for (@ARGV) {
     rcs_touch($_);
@@ -42,12 +49,21 @@ sub rcs_touch {
     unshift(@lines, "");
 
     for $i (1..$#lines) {
-	if ($lines[$i] =~ /^date\s+($re_date);/) {
-	    $old_date = $1;
-	    if (!defined($opt_r) || $lines[$i-1] =~ /^$opt_r/) {
-		print "$file: $i\n";
-		$lines[$i] =~ s/$old_date/$new_date/;
-		last;
+	next unless ($lines[$i-1] =~ /^($re_rev)$/);
+	next unless (!defined($opt_r) || $lines[$i-1] eq "$opt_r\n");
+	if ($lines[$i] =~ s/(^date\s+)($re_date);/$1$new_date;/) {
+	    print "$file: $i: $2 -> $new_date\n";
+	    $rev = $lines[$i-1];
+	    chop($rev);
+	    last
+	}
+    }
+    for $i (1..$#lines) {
+	if ($lines[$i] =~ /$re_id/) {
+	    if ($1 == $rev) {
+		$old_id = $3;
+		$lines[$i] =~ s/$old_id/$new_id/;
+		print "$file: $i: $old_id -> $new_id\n";
 	    }
 	}
     }
